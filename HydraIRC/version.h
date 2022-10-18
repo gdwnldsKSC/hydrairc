c954225b92dc73df37c66c91b59d564ae563b5b4
garydwnldsKSC <gary@kisaracorporation.com>
2022-10-18 02:54:02 -0400

    Upgrade to latest libXML
    Finally caught up on current libXML versions - that was quite a journey to get here!

--- HydraIRC/version.h
+++ HydraIRC/version.h
@@ -27,8 +27,8 @@
 
 */
 
-#define VERSIONNUM "v0.3.170"
-#define VERSIONDATE "(29/September/2022)"
+#define VERSIONNUM "v0.3.171"
+#define VERSIONDATE "(18/October/2022)"
 
 #ifdef RELEASE_VERSION
 #define VERSIONSTRING_EXTRA_1 ""
