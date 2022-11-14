02aa57e6a0941cd9ac7ca9bb0349d047fd02bd32
garydwnldsKSC <gary@kisaracorporation.com>
2022-11-13 22:00:56 -0500

    DNS Resolver IPv6 Friendly
    This changes the DNS resolver to use the newer future-proof host resolution function, which can return IPv6 addresses as well as IPv4, while still maintaining existing code compatibility.

--- HydraIRC/Common.cpp
+++ HydraIRC/Common.cpp
@@ -3,27 +3,17 @@
   HydraIRC
   Copyright (C) 2002-2006 Dominic Clifton aka Hydra
 
-  HydraIRC limited-use source license
-
-  1) You can:
-  1.1) Use the source to create improvements and bug-fixes to send to the
-       author to be incorporated in the main program.
-  1.2) Use it for review/educational purposes.
-
-  2) You can NOT:
-  2.1) Use the source to create derivative works. (That is, you can't release
-       your own version of HydraIRC with your changes in it)
-  2.2) Compile your own version and sell it.
-  2.3) Distribute unmodified, modified source or compiled versions of HydraIRC
-       without first obtaining permission from the author. (I want one place
-       for people to come to get HydraIRC from)
-  2.4) Use any of the code or other part of HydraIRC in anything other than 
-       HydraIRC.
-       
-  3) All code submitted to the project:
-  3.1) Must not be covered by any license that conflicts with this license 
-       (e.g. GPL code)
-  3.2) Will become the property of the author.
+  This program is free software: you can redistribute it and/or modify  
+  it under the terms of the GNU General Public License as published by  
+  the Free Software Foundation, version 3.
+ 
+  This program is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License 
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -42,7 +32,7 @@ CChildCommon *FindChildWindow(HWND hWnd);
 void IRCCommon::ProcessOutputBuffer( void )
 {
 #ifdef VERBOSE_DEBUG
-  sys_printf(BIC_FUNCTION,"IRCCommon::ProcessOutputBuffer()\n");
+  sys_Printf(BIC_FUNCTION,"IRCCommon::ProcessOutputBuffer()\n");
 #endif
   // process the buffer if we have text in it
   // but keep the buffer for later if the window's not open yet.
@@ -142,7 +132,7 @@ void IRCCommon::ProcessOutputBuffer( void )
     }
   }
 #ifdef VERBOSE_DEBUG
-  sys_printf(BIC_FUNCTION,"Exiting IRCCommon::ProcessOutputBuffer()\n");
+  sys_Printf(BIC_FUNCTION,"Exiting IRCCommon::ProcessOutputBuffer()\n");
 #endif
 }
 
